441ed093529a351a6bd1b402fc1e103c9857cc95
Aftermath001 <alvinasiachi@gmail.com>
2024-10-28 22:27:43 +0300

    Header

---
+++ backend/components/Header.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { RiBarChartHorizontalLine } from "react-icons/ri";
+import { GoScreenFull } from "react-icons/go";
+import { BiExitFullscreen } from "react-icons/bi";
+
+
+const Header = () => {
+    const [isFullscreen, setisFullscreen] = useState(false);
+    const toggleFullscreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen().then(()=>{
+                setisFullscreen(true);
+            })
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen().then(() => {
+                    setisFullscreen(false)
+                })
+                
+            } 
+            
+        }
+    }
+
+  return (
+    <>
+      <header className="header flex flex-sb">
+        <div className="logo flex gap-2">
+          <h1>ADMIN</h1>
+          <div className="headerham flex flex-center">
+            <RiBarChartHorizontalLine />
+          </div>
+        </div>
+
+        <div className="rightnav flex gap-2">
+          <div onClick={toggleFullscreen}>
+            {isFullscreen ? <BiExitFullscreen/> : <GoScreenFull/>}
+            
+          </div>
+          <div className="notification">
+            <img
+              src="/img/notification.png"
+              alt="notification"
+              className="w-16 h-16 rounded-full object-cover"
+            />
+          </div>
+          <div className="profilenav">
+            <img
+              src='/img/notification.png'
+              alt="notification"
+              className="w-16 h-16 rounded-full object-cover"
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
